bfef72a133c0baff4bd646d23b2b0e93b4161a7e
felipehscherer <felipe.scherepento@gmail.com>
2024-11-22 10:28:45 -0300

    ajuste do repositorio

--- SOBenchmarks/src/database/Database.java
+++
@@ -1,7 +0,0 @@
-package database;
-
-public abstract class Database {
-    public abstract int read(int position);
-    public abstract void write(int position, int value);
-    public abstract int sum();
-}
